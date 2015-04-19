f2fda8b29f9b87a595d1410dc06b7ea3eb8adebb
papachan <danloaiza@yahoo.com>
2015-04-19 08:11:51 -0500

    init repo

---
+++ src/etest_demo_app.erl
@@ -0,0 +1,16 @@
+-module(etest_demo_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    etest_demo_sup:start_link().
+
+stop(_State) ->
+    ok.
